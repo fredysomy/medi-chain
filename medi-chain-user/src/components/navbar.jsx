57e79e9442a57faa855d06d9b90c60ff36ef8b34
Jefrint <jefrintm@gmail.com>
2024-09-29 19:56:49 +0530

    Added Tailwind config and Directory in doctor and users

---
+++ medi-chain-user/src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+const Nav = () => {
+  return (
+    <div>Nav Bar</div>
+  )
+}
+export default Home; 
--- medi-chain-user/src/pages/Home.jsx
+++ medi-chain-user/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex justify-center '>Home Page for Doctor</div>
+    <div className='flex justify-center '>Home Page of Users</div>
   )
 }
 export default Home; 
