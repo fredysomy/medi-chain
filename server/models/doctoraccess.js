154cb7c0c0ed5c322069bd494c13f0f39d33ac54
fredysomy <fredysomy@gmail.com>
2024-10-17 00:57:48 +0530

    feart: added the models

---
+++ server/models/doctoraccess.js
@@ -0,0 +1,77 @@
+const {
+  DataTypes
+} = require('sequelize');
+module.exports = sequelize => {
+  const attributes = {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: true,
+      autoIncrement: true,
+      comment: null,
+      field: "id"
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "created_at"
+    },
+    acc_id: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "acc_id"
+    },
+    doc_id: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "doc_id"
+    },
+    user_id: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "user_id"
+    },
+    accessed_last: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "accessed_last"
+    },
+    access_given: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "access_given"
+    }
+  };
+  const options = {
+    tableName: "doctoraccess",
+    comment: "",
+    indexes: []
+  };
+  const DoctoraccessModel = sequelize.define("doctoraccess_model", attributes, options);
+  return DoctoraccessModel;
+};
---
+++ server/models/doctors.js
@@ -0,0 +1,68 @@
+const {
+  DataTypes
+} = require('sequelize');
+module.exports = sequelize => {
+  const attributes = {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: true,
+      autoIncrement: true,
+      comment: null,
+      field: "id"
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "created_at"
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "name"
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "email"
+    },
+    orgid: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "orgid"
+    },
+    uuid: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "uuid"
+    }
+  };
+  const options = {
+    tableName: "doctors",
+    comment: "",
+    indexes: []
+  };
+  const DoctorsModel = sequelize.define("doctors_model", attributes, options);
+  return DoctorsModel;
+};
