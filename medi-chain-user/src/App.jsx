57668df77c15a67c3fce70cc056b53e4f29d14fe
Jeslin Kuriakose <jeslinkuriakose03@gmail.com>
2024-11-03 20:39:28 +0530

    create account page added

--- medi-chain-user/src/App.jsx
+++ medi-chain-user/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home"
 import Login from "./pages/login";
+import Create from "./pages/create";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/login"   element={<Login/>} />
+          <Route path="/create"  element={<Create/>} />
          
           
         
